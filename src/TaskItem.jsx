6f77988530f17149ecdefdbc8bde9ba3772af837
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2023-10-31 13:45:00 +0330

    #23 conditional-rendering--form

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -3,27 +3,33 @@ import { TaskContext } from "./TaskContext";
 
 const TaskItems = () => {
   const { TaskItems, setTaskItems } = useContext(TaskContext);
-  return (
-    <ul className="list-group m-0 p-0 mt-2">
-      {TaskItems.map((index) => (
-        <li
-          className={`list-group-item d-flex justify-content-between ${
-            index.done ? "list-group-item-success" : ""
-          }`}
-        >
-          {index.title}
-          <span>
-            {index.done ? (
-              <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-            ) : (
-              <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-            )}
-            <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-          </span>
-        </li>
-      ))}
-    </ul>
-  );
+  if (TaskItems.length) {
+    return (
+      <ul className="list-group m-0 p-0 mt-2">
+        {TaskItems.map((index) => (
+          <li
+            className={`list-group-item d-flex justify-content-between ${
+              index.done ? "list-group-item-success" : ""
+            }`}
+          >
+            {index.title}
+            <span>
+              {index.done ? (
+                <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
+              ) : (
+                <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
+              )}
+              <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <h4 className="text-center text-danger mt-4">هیچ کاری ثبت نشد...!</h4>
+    );
+  }
 };
 
 export default TaskItems;
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,14 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useState } from "react";
+import { TaskContext } from "./TaskContext";
 
 const TopForm = () => {
+  const [Task, setTask] = useState("");
+  const { TaskItems, setTaskItems } = useContext(TaskContext);
+
+  const handleSetTask = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    setTaskItems([
+      ...TaskItems,
+      { id: Math.random(), title: Task, done: false },
+    ]);
+    setTask("");
+  };
+
   return (
     <Fragment>
       <h4 className="text-center text-info text_shdow">
         فعالیت های خود را وارد کنید
       </h4>
-      <form>
+      <form onSubmit={handleAddTask}>
         <div className="form-group d-flex">
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={Task}
+            onChange={handleSetTask}
+          />
           <button type="submit" className="btn btn-success me-1">
             ثبت
           </button>
