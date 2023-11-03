a6966164b9268eb015e564b27a1e4ac471e94d4a
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2023-11-03 23:19:54 +0330

    24 react-todo-project (Delete-!Done)

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -3,23 +3,41 @@ import { TaskContext } from "./TaskContext";
 
 const TaskItems = () => {
   const { TaskItems, setTaskItems } = useContext(TaskContext);
+  const handleSetDoneTask = (id) => {
+    const index = TaskItems.findIndex((t) => t.id == id);
+    let NewTaskItems = [...TaskItems];
+    NewTaskItems[index].done = !NewTaskItems[index].done;
+    setTaskItems(NewTaskItems);
+  };
+  const handleSetDeleteTask = (id) => {
+    const NewTask = TaskItems.filter((t) => t.id != id);
+    setTaskItems(NewTask);
+  };
   if (TaskItems.length) {
     return (
       <ul className="list-group m-0 p-0 mt-2">
-        {TaskItems.map((index) => (
+        {TaskItems.map((t) => (
           <li
             className={`list-group-item d-flex justify-content-between ${
-              index.done ? "list-group-item-success" : ""
+              t.done ? "list-group-item-success" : ""
             }`}
           >
-            {index.title}
+            {t.title}
             <span>
-              {index.done ? (
-                <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-              ) : (
-                <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-              )}
-              <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+              <i
+                className={`me-3 pointer fas ${
+                  t.done ? "fa-times text-warning" : "fa-check text-success"
+                } transition_200 text_hover_shadow`}
+                onClick={() => {
+                  handleSetDoneTask(t.id);
+                }}
+              ></i>
+              <i
+                className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"
+                onClick={() => {
+                  handleSetDeleteTask(t.id);
+                }}
+              ></i>
             </span>
           </li>
         ))}
